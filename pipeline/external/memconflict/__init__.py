ca96a83527aec7230832fdf8ed36fa09f980854e
haruhi <haruhisuzimiya0@gmail.com>
2026-06-20 15:38:23 +0800

    Update Memconflict adapter

---
+++ pipeline/external/memconflict/__init__.py
@@ -0,0 +1,2 @@
+"""MemConflict external benchmark adapter."""
+
---
+++ pipeline/external/memconflict/adapters/__init__.py
@@ -0,0 +1,6 @@
+from __future__ import annotations
+
+from pipeline.external.memconflict.adapters.registry import TASK_TYPES, get_adapter
+
+__all__ = ["TASK_TYPES", "get_adapter"]
+
